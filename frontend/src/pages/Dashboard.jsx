661e860dfa7d44653d49b9a94d9dcbb99206b032
Yamil-Layicov <yamilbl@code.edu.az>
2023-03-19 14:21:46 +0400

    day-4

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,6 @@
-import { useEffect } from "react"
-import {useNavigate} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import React from 'react'
 
 const Dashboard = () => {
-  const navigate = useNavigate()
-
-  const {user} = useSelector((state) => state.auth)
-
-  useEffect(() => {
-    if(!user){
-      navigate('/login')
-    }
-  } ,[user,navigate])
-  
   return (
     <div>Dashboard</div>
   )
